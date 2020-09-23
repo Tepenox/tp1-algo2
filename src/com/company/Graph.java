9e84ed38e1d5d672f4306fc0976e3f57dd049abe
Tepenox <anasselafya@gmail.com>
2020-09-23 10:02:00 +0200

    graph d implications done

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -1,3 +1,5 @@
+package com.company;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -37,16 +39,18 @@ public class Graph<Label> {
 
     public String toString() {
         String result = new String("");
-        result.concat(cardinal + "\n");
+        result = result.concat(cardinal + "\n");
         for (int i = 0; i<cardinal;i++) {
             for (Edge e : incidency.get(i)) {
-                result.concat(e.source + " " + e.destination + " "
+                result = result.concat(e.source + " " + e.destination + " "
                         + e.label.toString() + "\n");
             }
         }
         return result;
 
     }
+
+
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,32 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<List<Integer>> pairs =  new ArrayList<>();
+        pairs.add(Arrays.asList(1 , -2));
+        pairs.add(Arrays.asList(-1 , 3));
+        pairs.add(Arrays.asList(1 , 3));
+        pairs.add(Arrays.asList(-2 , -3));
+        System.out.println(makeGraph(pairs));
+    }
+
+    public static Graph<String> makeGraph(List<List<Integer>> pairs) {
+        Graph<String> graph = new Graph<>(6);
+        int graphOrder = graph.order();
+        for (List<Integer> pair : pairs) {
+            graph.addArc(intLabelToIndex(Math.negateExact(pair.get(0)), graphOrder) , intLabelToIndex(pair.get(1), graphOrder)
+                    , Math.negateExact(pair.get(0)) + "->" + pair.get(1));
+            graph.addArc(intLabelToIndex(Math.negateExact(pair.get(1)),graphOrder) , intLabelToIndex(pair.get(0),graphOrder)
+                    , Math.negateExact(pair.get(1)) + "->" + pair.get(0));
+        }
+        return graph;
+
+    }
+
+    public static int intLabelToIndex(int intLabel , int order) {
+        return intLabel > 0 ? intLabel + (order /2 ) - 1 : intLabel + (order / 2);
     }
 }
